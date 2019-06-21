28fc9f555c8f7d4d194521958d67038839deedfe
tmoynandy <tmoy.nandy@gmail.com>
2019-06-21 16:40:39 +0530

    added signin & signup components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Navbar from './components/layout/Navbar'
 import Dashboard from './components/dashboard/Dashboard'
 import ProjectDetails from './components/project/ProjectDetails'
+import SignIn from './components/auth/SignIn'
+import SignUp from './components/auth/SignUp'
+
 
 
 class App extends Component {
@@ -14,6 +17,8 @@ class App extends Component {
           <Switch>
             <Route exact path = '/' component = {Dashboard} />
             <Route path = '/project/:id' component = {ProjectDetails} />
+            <Route path = '/signin' component = {SignIn} />
+            <Route path = '/signup' component = {SignUp} />
           </Switch>
         </div>
       </BrowserRouter>
