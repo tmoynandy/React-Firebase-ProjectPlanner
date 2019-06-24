950d5338b09d83e3f7c11573bf7dc53f60522d4d
tmoynandy <tmoy.nandy@gmail.com>
2019-06-24 18:15:42 +0530

    Finished all components | added basic styling

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -4,8 +4,8 @@ import {NavLink} from 'react-router-dom'
 const SignedInLinks = () =>{
     return (
         <ul className="right">
-            <li><NavLink to = '/'>New Project</NavLink></li>
-            <li><NavLink to = '/'></NavLink>Logout</li>
+            <li><NavLink to = '/create'>New Project</NavLink></li>
+            <li><NavLink to = '/'>Logout</NavLink></li>
             <li><NavLink to = '/' className = 'btn btn-floating pink lighten-1'>Tanu</NavLink></li>
         </ul>
     )
--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux'
+import rootReducer from './store/reducers/rootReducer'
+
+const store = createStore(rootReducer)
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
