3d4373bcb923e8f22727ff93afc11786fddd732f
tmoynandy <tmoy.nandy@gmail.com>
2019-06-24 18:18:56 +0530

    added store, reducers, provider

--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {createStore} from 'redux'
 import rootReducer from './store/reducers/rootReducer'
-
+//enables application's access to store
+import {Provider} from 'react-redux' 
 const store = createStore(rootReducer)
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
