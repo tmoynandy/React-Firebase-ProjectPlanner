d4f610b1987a6944c4a82624be0301cb0974ac4c
tmoynandy <tmoy.nandy@gmail.com>
2019-06-26 01:15:48 +0530

    added logout

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
+import {connect} from 'react-redux'
+import {signOut} from '../../store/actions/authActions'
 
-const SignedInLinks = () =>{
+const SignedInLinks = (props) =>{
+
     return (
         <ul className="right">
             <li><NavLink to = '/create'>New Project</NavLink></li>
-            <li><NavLink to = '/'>Logout</NavLink></li>
+            <li><a onClick={props.signOut}>Logout</a></li>
             <li><NavLink to = '/' className = 'btn btn-floating pink lighten-1'>Tanu</NavLink></li>
         </ul>
     )
 }
 
-export default SignedInLinks
+const mapDispathToProps = (dispatch) =>{
+    return {
+        signOut : () => dispatch(signOut())
+    }
+}
+
+export default connect(null, mapDispathToProps)(SignedInLinks)
