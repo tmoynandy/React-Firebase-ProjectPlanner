61627f36154c5b0644dbf5dc3a4e41dc141ad250
tmoynandy <tmoy.nandy@gmail.com>
2019-06-26 00:57:02 +0530

    added user signin

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import {connect} from 'react-redux'
+import {signIn} from '../../store/actions/authActions'
 
 class SignIn extends Component {
     state = {
@@ -13,9 +15,11 @@ class SignIn extends Component {
     }
     handleSubmit = (e) => {
         e.preventDefault();
-        console.log(this.state)
+        //console.log(this.state)
+        this.props.signIn(this.state)
     }
     render() {
+        const {authError} = this.props
         return (
             <div className="container">
                 <form onSubmit={this.handleSubmit} className="white">
@@ -30,11 +34,26 @@ class SignIn extends Component {
                     </div>
                     <div className="input-field">
                         <button className="btn pink lighten-1 z-depth-0">Login</button>
+                        <div className="red-text center">
+                            {authError ? <p>{authError}</p> : null}
+                        </div>
                     </div>
                 </form>
             </div>
         )
     }
 }
+const mapStateToProps = (state) =>{
+    return {
+        authError : state.auth.authError
+    }
+}
 
-export default SignIn
+const mapDispatchToProps = (dispatch) =>{
+    return {
+        signIn : (creds) => dispatch(signIn(creds))
+    }
+}
+
+
+export default connect(mapStateToProps,mapDispatchToProps)(SignIn)
